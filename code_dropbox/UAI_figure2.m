57569a0df96c83c01c4ce4fbc64082084012bc0e
P.K. Rubenstein <pkr23@explorer.eng.cam.ac.uk>
2016-02-24 20:09:27 +0000

    figure 1

--- code_dropbox/UAI_figure2.m
+++ code_dropbox/UAI_figure2.m
@@ -1,6 +1,6 @@
 clear all
 
-dependence_coefficients = [0.1,0.3,0.5,0.6,0.7,0.8,0.9];
+dependence_coefficients = 0.1:0.02:0.9;
 %first column for bootstrap, second columns for permutation
 rejections = zeros(length(dependence_coefficients),2);%for alpha=0.05
 %also record all of the pvalues in case we want to do something later
@@ -9,7 +9,7 @@ for j=1:length(dependence_coefficients);
     fprintf('Simulating data with dependence coeff:')
     fprintf(num2str(dependence_coefficients(j)))
     fprintf('\n------------------------\n')
-    numexps = 500;
+    numexps = 200;
     
     num_rejects_Lancaster_naive = 0;
     num_rejects_Lancaster_perm = 0;
@@ -39,14 +39,14 @@ save('UAI_figure2','tosave')
 
 h=figure;
 
-plot(dependence_coefficients,rejections(:,1),'-o')
+plot(dependence_coefficients,rejections(:,1),'-ob')
 xlabel('Dependence coefficient')
 ylabel('Type I error')
 title('False positives - Wild Bootstrap vs Permutation')
 
 hold
 
-plot(dependence_coefficients,rejections(:,2),'-o')
+plot(dependence_coefficients,rejections(:,2),'-or')
 
 
 legend('Lancaster with Wild Bootstrap','Lancaster with Permutation Bootstrap','Location','NorthWest')
@@ -62,5 +62,5 @@ set(gcf, 'PaperUnits','centimeters');
 set(gcf, 'PaperSize', [pos(3)+ti(1)+ti(3) pos(4)+ti(2)+ti(4)]);
 set(gcf, 'PaperPositionMode', 'manual');
 set(gcf, 'PaperPosition',[0 0 pos(3)+ti(1)+ti(3) pos(4)+ti(2)+ti(4)]);
-saveas(h,'figures/wildBootstrap_is_necessary.pdf');
-saveas(h,'figures/wildBootstrap_is_necessary.fig');
+saveas(h,'figures/UAI_Figure2.pdf');
+saveas(h,'figures/UAI_Figure2.fig');
